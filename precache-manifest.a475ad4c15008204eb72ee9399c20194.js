dcaef57e10aca3b6ad6be7ab961618eac16f6fcf
Maria <you@example.com>
2019-03-18 22:16:49 +0300

    Visualisation line chart on random data

---
+++ precache-manifest.a475ad4c15008204eb72ee9399c20194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e88a47f4b40095a731",
+    "url": "/static/css/main.dd43cedc.chunk.css"
+  },
+  {
+    "revision": "08e88a47f4b40095a731",
+    "url": "/static/js/main.ce671415.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "e01e2cd72f4a5a177507326a4cf11d8a",
+    "url": "/index.html"
+  }
+];
